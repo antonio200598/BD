776d7a83fb335580b8c125bd8e04e48aaa0d16d9
Antônio Maurício <antoniomauricio3469@gmail.com>
2022-07-03 16:31:02 -0300

    atualização do código

--- src/main/java/com/example/bd/StudentManager.java
+++ src/main/java/com/example/bd/StudentManager.java
@@ -1,7 +1,20 @@
 package com.example.bd;
 
 public class StudentManager {
+    public int addStudent(Student student){
+        
+    }
+    public List<Student> getStudents(){
+
+    }
+    public Students updateStudent(Student student){
+
+    }
     
+    public Students deleteStudent(Student student){
+
+    }
+
     
 
 
